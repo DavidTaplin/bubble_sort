f38d1e76f173e574ec7d441d8a2204d62e83b553
DavidTaplin <DTaplin616@live.com>
2023-01-16 17:26:34 -0600

    Initial commit

---
+++ bubblesort.rb
@@ -0,0 +1,24 @@
+bubble_array = [4,3,78,2,0,2]
+
+def bubble_sort(array)
+    unsorted = true
+    # while the array is unsorted
+    while unsorted do
+      # loop through the array
+      i = 0
+      unsorted = false
+      while i < (array.length - 1)
+        # if the value of the current index is greater than the next index
+        if array[i] > array[i + 1]
+          # set the value of the current index to the value of the next index & the value of the next index to the value of the current index
+          array[i], array[i + 1] = array[i + 1], array[i]
+          unsorted = true
+        end
+        i += 1
+      end
+    end
+    # return array
+    p array
+  end
+  
+  p bubble_sort(bubble_array)
